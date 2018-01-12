8fa086b70a3c8db99e1351d9182b4421b5037687
sijacinto <sijacinto@yahoo.com>
2018-01-12 01:27:56 -0200

    Add files via upload

---
+++ first2018.R
@@ -0,0 +1,21 @@
+a<-2
+b<-3
+max(a,b)
+alpha<-0.01
+tsa <-read.table("C:/Users/sijac/Documents/Artigos_Adri/Journal/Fscore.csv",header=TRUE)
+
+tsa
+alpha
+plot(tsa)
+#Publishing your R code on GitHub 
+cd	/Users/sijac/Documents/Pesquisa/R_testes	
+github	add .
+.git	commit -m	"First	commit”	
+git	remote	add	origin	https://github.com/sijacinto/R_Testes.git	
+git	push	-u	origin	master	
+Check https://github.com/sijacinto/R_Testes
+#rePublishing your R code on GitHub 
+ git	add	.	
+  git	commit	-m	“Nth	commit”	
+  git	push	-u	origin	master	
+  Check https://github.com/sijacinto/R_Testes	 
